efa8c940475462776be5241ff3b046b69eebe1ba
rainierwolfcastle <das.bobby2000@gmail.com>
2016-04-01 14:24:52 +0100

    fix for heroku

--- websockets/settings.py
+++ websockets/settings.py
@@ -127,12 +127,12 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'chat/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 CHANNEL_LAYERS = {
